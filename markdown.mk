860cc28b403358600a2b220008ad10026d2168e1
zml <zml@aoeu.xyz>
2016-01-24 23:51:57 -0800

    Support md -> tex conversion using minted for syntax highlighting

--- markdown.mk
+++ markdown.mk
@@ -6,3 +6,6 @@ PANDOC_PDF_OPTS += -V geometry:margin=1in -V geometry:top=0.5in
 
 %.pdf: %.md
 	pandoc $< -o $@ $(PANDOC_OPTS) $(PANDOC_PDF_OPTS) --standalone
+
+%.tex: %.md
+	pandoc $< -o $@ $(PANDOC_OPTS) --no-highlight --filter $(MAKEFILE_BASE)pandocminted/minted.py
