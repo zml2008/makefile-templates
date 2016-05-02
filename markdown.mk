96f91f040e004f3ceefe1324c13567ffa1386aa7
zml <zml@aoeu.xyz>
2016-05-02 14:25:45 -0700

    Add initial support for pandoc-markdown presentations

--- markdown.mk
+++ markdown.mk
@@ -9,3 +9,9 @@ PANDOC_PDF_OPTS += -V geometry:margin=1in -V geometry:top=0.5in
 
 %.tex: %.md
 	pandoc $< -o $@ $(PANDOC_OPTS) --no-highlight --filter $(MAKEFILE_BASE)pandocminted/minted.py
+
+%.html: %.present.md
+	pandoc -t slidy --standalone $< -o $@ $(PANDOC_OPTS) $(PANDOC_HTML_OPTS) $(PANDOC_PRESENTATION_OPTS)
+
+%.pdf: %.present.md
+	pandoc -t beamer $< -o $@ $(PANDOC_OPTS) $(PANDOC_PRESENTATION_OPTS)
