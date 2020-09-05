5164e5b93b9e225626e026b8d993be3c315521a9
zml <zml@aoeu.xyz>
2020-09-04 19:03:47 -0700

    markdown: Use revealjs instead

--- markdown.mk
+++ markdown.mk
@@ -11,7 +11,7 @@ PANDOC_PDF_OPTS += -V geometry:margin=1in -V geometry:top=0.5in
 	pandoc $< -o $@ $(PANDOC_OPTS) --no-highlight --filter $(MAKEFILE_BASE)pandocminted/minted.py
 
 %.html: %.present.md
-	pandoc -t slidy --standalone $< -o $@ $(PANDOC_OPTS) $(PANDOC_HTML_OPTS) $(PANDOC_PRESENTATION_OPTS)
+	pandoc -t revealjs --standalone $< -o $@ $(PANDOC_OPTS) $(PANDOC_HTML_OPTS) $(PANDOC_PRESENTATION_OPTS)
 
 %.pdf: %.present.md
 	pandoc -t beamer $< -o $@ $(PANDOC_OPTS) $(PANDOC_PRESENTATION_OPTS)
